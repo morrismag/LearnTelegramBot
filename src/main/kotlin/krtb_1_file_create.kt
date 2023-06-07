5e82f41a808b5e4970a9f26ad72b3ab45dbae668
morrismag <morrissmag@gmail.com>
2023-06-07 16:19:40 +0300

    KRTB-2-Parse and dictionary

--- src/main/kotlin/krtb_1_file_create.kt
+++ src/main/kotlin/krtb_1_file_create.kt
@@ -1,16 +1,32 @@
 import java.io.File
 
+data class Word(
+    var wordEnglish: String = "",
+    var wordRussian: String = "",
+    var correctAnswersCount: Int = 0
+)
+
 fun main() {
     val wordsFile = File("word.txt")
     wordsFile.createNewFile()
     wordsFile.writeText(
-        "hello привет\n" +
-                "dog собака\n" +
-                "cat кошка\n"
+        "hello|привет\n" +
+                "dog|собака|1\n" +
+                "cat|кошка\n"
     )
-    val listString = wordsFile.readLines()
+    val listString: List<String> = wordsFile.readLines()
+    val dictionary = mutableListOf<Word>()
 
-    for (i in listString.indices) {
-        println(listString[i])
+    for (line in listString) {
+        val wordList = line.split("|")
+        val word = Word(
+            wordEnglish = wordList[0],
+            wordRussian = wordList[1],
+            correctAnswersCount = if (wordList.count() >= 3) wordList[2].toInt()
+            else 0
+        )
+
+        dictionary.add(word)
     }
+    println(dictionary)
 }
