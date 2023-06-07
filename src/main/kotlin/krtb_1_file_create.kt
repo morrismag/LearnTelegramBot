02efedf1cac486ee1e96360f410219a358e16e72
morrismag <morrissmag@gmail.com>
2023-06-07 12:47:59 +0300

    KRTB-1-Create and read file

--- src/main/kotlin/krtb_1_file_create.kt
+++ src/main/kotlin/krtb_1_file_create.kt
@@ -3,11 +3,14 @@ import java.io.File
 fun main() {
     val wordsFile = File("word.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n" +
-            "dog собака\n" +
-            "cat кошка\n")
-
+    wordsFile.writeText(
+        "hello привет\n" +
+                "dog собака\n" +
+                "cat кошка\n"
+    )
     val listString = wordsFile.readLines()
-    for (i in listString.indices){
-    println(listString[i])}
+
+    for (i in listString.indices) {
+        println(listString[i])
+    }
 }
